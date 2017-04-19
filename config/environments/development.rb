b07dc278d65e0ec54a00546ae66b7a4b1f9dc9a2
ryuya tosaka <ryu.happy9@gmail.com>
2017-04-19 12:45:52 +0900

    Edit development aws config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,4 +53,15 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.paperclip_defaults = {
+  storage: :s3,
+  s3_protocol: :https,
+  s3_credentials: {
+    bucket: ENV['S3_BUCKET_NAME'],
+    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    s3_region: ENV['AWS_REGION'],
+  }
+  }
 end
